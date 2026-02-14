5fc0168870e5a1be15eb3f1c0ff0fa59b32331ee
Charlotte <email@armandalvarez.com>
2026-02-14 14:49:20 -0500

    test(problem): Added tests for problem 1

---
+++ tests/test_1.py
@@ -0,0 +1,47 @@
+# Test file for problem 001
+from problem_001.submission import answer
+import sys
+from io import StringIO
+
+
+def test() -> tuple:
+    """ 
+    Test for problem. 
+
+    Args:
+        None
+
+    Returns:
+        tuple: The number of passing tests, the total number of tests
+    """
+
+    correct = 0
+    num_tests = 0
+    for test_func in [test_1]:
+        if test_func():
+            correct += 1
+        num_tests += 1
+
+    return (correct, num_tests)
+    
+
+def test_1() -> bool:
+    try:
+        # Capture stdout
+        old_stdout = sys.stdout
+        sys.stdout = StringIO()
+        
+        answer()
+        
+        output = sys.stdout.getvalue()
+        sys.stdout = old_stdout
+        
+        assert output.startswith("Hello Field Engineers")
+        return True
+    except (AssertionError, Exception):
+        sys.stdout = old_stdout
+        return False
+
+
+if __name__ == "__main__":
+    test()
