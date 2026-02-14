b74a8bd3e7cab562077b43f1d70951ae21bdf930
Charlotte <email@armandalvarez.com>
2026-02-14 14:48:55 -0500

    feat(problem): Added problem 1

---
+++ problem_001/problem_statement.md
@@ -0,0 +1,5 @@
+# 1 - Hello Field Engineers
+
+In this problem, your task is to simply print 'Hello Field Engineers'.
+
+> Note: Capitalization is important for this problem. 
---
+++ problem_001/submission.py
@@ -0,0 +1,17 @@
+# Write your code in the function labeled `problem()` below
+###########################################################
+
+def answer() -> None:
+    """
+    This function prints 'Hello Field Engineers'
+
+    Args:
+        None
+    Return:
+        None
+    """
+
+    ## Write your code here
+
+
+    return
